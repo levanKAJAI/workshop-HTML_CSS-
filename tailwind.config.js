4e15aca398066324041dcb21d70da069090e1cc0
levan kajaia <133898764+levanKAJAI@users.noreply.github.com>
2026-04-23 14:55:10 +0400

    add some files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>;
